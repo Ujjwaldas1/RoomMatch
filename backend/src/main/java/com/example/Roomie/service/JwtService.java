af7acad9af8d6605a3ec3a3efb256bd31bf6eadd
Ujjwal Das <ujjwaldas854104@gmail.com>
2026-05-28 02:04:50 +0530

    Cleanup + matching integration fixes

--- backend/src/main/java/com/example/Roomie/service/JwtService.java
+++ backend/src/main/java/com/example/Roomie/service/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
